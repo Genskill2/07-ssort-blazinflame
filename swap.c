b8cb51f6f76d07b2d1fbc7d6471302e40ed4b661
blazinflame <84576462+blazinflame@users.noreply.github.com>
2021-06-26 12:55:48 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -2,15 +2,14 @@ void swap_max(int x[], int l, int n);
 {
   int i,k,j;
   k=x[n];
-  
+   j=k;
     for (i=n; i<l; i++)
     {
       if(k<x[i])
-        j=k;
+       k=x[i];
     }      
-     
       x[i]=j;
-return 0;
+     x[n]=k;
 }
 
 void ssort(int x[], int  l); //making the selection sort function//
