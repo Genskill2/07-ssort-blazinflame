a95cfde5857a5e055c2314200b5493cfb613e308
blazinflame <84576462+blazinflame@users.noreply.github.com>
2021-06-26 13:17:30 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,20 +1,22 @@
-void swap_max(int arr[], int l, int n)
+void swap_max(int z[], int l, int n)
 {
-	int i, lar=arr[n], p=n, t;
-	for(i=n;i<l;i++)
-		if(lar<arr[i])
+	int i, j;
+	int k=x[n]; 
+	int a=n; 
+	for(i=n; i<l; i++)
+		if(k<x[i])
 		{
-			p=i;
-			lar = arr[i];
+			a = i;
+			k = x[i];
 		}
-	t = arr[n];
-	arr[n] = arr[p];
-	arr[p] = t;
+	j = x[n];
+	x[n] = x[a];
+	x[a] = j;
 }
 
-void ssort(int arr[], int l)
+void ssort(int x[], int l)
 {
 	int i;
 	for(i=0;i<l;i++)
-		swap_max(arr, l, i);
+		swap_max(x, l, i);
 }
